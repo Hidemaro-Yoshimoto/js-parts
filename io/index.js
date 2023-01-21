d4ceeeb4810a0cae29464d35f106c26d62d75029
Hidemaro-Yoshimoto <bsnow83ball@gmail.com>
2023-01-21 13:31:12 +0900

    ioの練習

---
+++ io/index.js
@@ -0,0 +1,50 @@
+let imagesItems = [...document.querySelectorAll(".img-wrap")];
+let titles = [...document.querySelectorAll("h2")];
+let titleMessage = document.querySelector(".title");
+
+const setItemActive = (entries) => {
+  entries.map((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    } else {
+      entry.target.classList.remove("active");
+    }
+  });
+};
+
+const options = {};
+
+// const observer = new IntersectionObserver(setItemActive, options);
+// observer.observe(titleMessage);
+
+// img-wrapは偶数と奇数で出現する場所が違う
+// imagesItems.map((item, index) => {
+//   item.children[0].style.backgroundImage = "url(./imgs/sectionImg.jpg)";
+//   index % 2 === 0 ? (item.style.left = "55%") : (item.style.left = "5%");
+//   observer.observe(item);
+// });
+
+// titles.map((title, index) => {
+//   index % 2 === 0 ? title.style.left = '35%' : title.style.left = '10%';
+//   observer.observe(title)
+// })
+
+class Observer {
+  constructor(imgEls, textEls) {
+    this.imageItems = [...document.querySelectorAll(imgEls)];
+    this.titles = [...document.querySelectorAll(textEls)];
+    // this.titleMessage = document.querySelector('')
+    
+    this.displayImage();
+  }
+  
+  displayImage() {
+    this.imageItems.map((item, index) => {
+      item.children[0].style.backgroundImage = "./imgs/sectionImg.jpg";
+      index % 2 === 0 ? (item.style.left = "55%") : (item.style.left = "5%");
+      observer.observe(item)
+    });
+  }
+}
+
+const observer = new IntersectionObserver(".img-wrap", "h2");
